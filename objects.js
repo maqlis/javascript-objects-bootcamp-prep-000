cf12b94a91ef47ccdf9a3771e85450a724c69770
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-23 08:34:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
-var playlistt = {}
-playlistt.dido = "Life for Rent"
+var playlist = {}
+playlist.dido = "Life for Rent"
 
 function updatePlaylist(playlist, artist, song){
   return Object.assign({}, playlist, { [artist]: song })
